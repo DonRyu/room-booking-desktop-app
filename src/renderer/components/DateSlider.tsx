d6183e02bf50dbb92b43dda9a0236dabb9fe5a44
don <donryu1031@gmail.com>
2023-10-24 11:21:53 -0700

    dayjs

--- src/renderer/components/DateSlider.tsx
+++ src/renderer/components/DateSlider.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import dayjs from 'dayjs';
 
 interface Props {}
 
@@ -9,13 +10,27 @@ function DateSlider(props: Props) {
 
   const num_Days = useSelector((state: RootState) => state.days);
 
-  const showDate = () => {
-    const today = new Date();
-    const startDate = new Date(today);
+  function generateDateArray(buttonCount: number): number[] {
+    const today = dayjs(); // 1일로 가정
+    const days = [];
 
-    return 0;
-  };
+    // for (let i = 0; i < buttonCount; i++) {
+    //   const date = today.add(i, 'day');
 
+    //   // 현재 날짜의 월과 다음 날짜의 월을 비교하여 윤년 여부를 확인
+    //   const isCurrentDateLeapYear = date.isLeapYear();
+    //   const isNextDateLeapYear = date.add(1, 'day').isLeapYear();
+
+    //   // 다음 날짜가 윤년인 경우 2월은 29일까지, 아닌 경우 2월은 28일까지
+    //   if (date.month() === 1 && isNextDateLeapYear) {
+    //     days.push(29);
+    //   } else {
+    //     days.push(date.date());
+    //   }
+    }
+
+    return days;
+  }
   return <div>{showDate()}</div>;
 }
 
