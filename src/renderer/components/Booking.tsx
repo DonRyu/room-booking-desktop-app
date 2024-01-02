fd38170ebbec1a091ff3d6ab0fb406e372a58204
don <donryu1031@gmail.com>
2024-01-02 14:33:38 -0800

    add

--- src/renderer/components/Booking.tsx
+++ src/renderer/components/Booking.tsx
@@ -6,61 +6,36 @@ import { BookingBtn, DateBtn } from '../style';
 import dayjs from 'dayjs';
 import { Room } from '../../types';
 
-// [
-//   {
-//       "id": 4,
-//       "type": "Hold",
-//       "room_number": 101,
-//       "student_number": 12345,
-//       "student_name": "Don Ryu",
-//       "sex": 1,
-//       "check_in": "2023-01-01",
-//       "check_out": "2023-01-10",
-//       "nationality": "USA",
-//       "marketer": "Marketing Team",
-//       "created_date": "2023-01-01"
-//   }
-// ]
+interface days {
+  ddd: string;
+  dd: string;
+}
 
-// 1                 15
-// 1 2 3 4 5 6 7 8 9 (체크아웃 날짜는 포함하지 않음)
-
-// {
-//
-//   booking: [
-//     room: 20223,
-//     {
-//
-//       date: 1,
-//       user: 18,
-//       type: 2,
-//     },
-//     {
-//
-//       date: 2,
-//       user: 18,
-//       type: 2,
-//     },
-//   ]
-// }
-
-function Booking({ days }: any) {
+function Booking({ days }: { days: days[] }) {
   const num_Days = useSelector((state: RootState) => state.days);
   const roomDataArr = useSelector((state: RootState) => state.roomData);
 
-
-  // console.log('roomDataArr', roomDataArr.booking[0].date);
+  console.log('roomDataArr', roomDataArr); // {room_number: 101, booking: Array(9)}
+  console.log('days', days); // [{ddd: 'Sun', dd: '31'}]
   return (
     <div style={{ height: '95%' }}>
       <div style={{ display: 'flex' }}>
-        <BookingBtn>1</BookingBtn>
-        {days.map((item: any, key: number) => {
-          return (
-            <BookingBtn key={key}>
-
-            </BookingBtn>
-          );
-        })}
+        <div>
+          {roomDataArr.map((room: any, key: number) => {
+            return (
+              <div style={{ display: 'flex' }}>
+                <BookingBtn>{room.room_number}</BookingBtn>
+                {days.map((day,key) => {
+                  return (
+                    <BookingBtn>
+                      {day.dd === room.booking[key]?.date ? 1: 'a22d'}
+                    </BookingBtn>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
